154dc562d714821a8824c9ba69e43db3047b26fb
KianaMsqt <masqati.kiana@gmail.com>
2023-03-09 20:53:46 +0000

    - Install axios - Create a snippet of requesting for job search from jsearch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  
+  // Request to jsearch and print the results on the console
+  const options = {
+    method: 'GET',
+    url: 'https://jsearch.p.rapidapi.com/search',
+    params: {
+      query: 'Python developer in Texas, USA',
+      num_pages: '1'
+    },
+    headers: {
+      'X-RapidAPI-Key': '28a99368c8msh158d941b1a4bd17p16d687jsn5202245154cb',
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
