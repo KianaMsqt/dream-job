954d882a15512a8989ba1f7c1cb9d58b70360e16
Ope <o_orekoya@yahoo.com>
2023-03-14 20:54:29 +0000

    add email popup and contact

--- src/components/job_search/JobSearch.jsx
+++ src/components/job_search/JobSearch.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function JobSearch() {
-  return <h1 className="jobSearch"></h1>;
+  return <h1 className="jobSearch">Job Search</h1>;
 }
 
 export default JobSearch;
