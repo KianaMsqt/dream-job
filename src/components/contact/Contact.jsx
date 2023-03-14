cc6f6ab5c2a9b1081442ca05c113387d13517955
Ope <o_orekoya@yahoo.com>
2023-03-14 20:49:29 +0000

    add email popup and contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,24 @@
+import { CenterFocusStrong } from "@mui/icons-material";
+import { TextField, Button, Box } from "@mui/material";
 
-function Contact(){
-
+function Contact() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1, width: "30ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField style={{marginLeft: "15%" }} label="Full Name" />
+      <TextField label="Email" />
+      <Button variant="contained">Submit</Button>
+      <br />
+      <TextField label="Message" style = {{width: 1000, marginLeft: "10%" }} multiline rows={5} />
+      
+    </Box>
+  );
 }
 
 export default Contact;
--- src/components/email_popup/EmailPopup.jsx
+++ src/components/email_popup/EmailPopup.jsx
@@ -1,5 +1,22 @@
-function EmailPopup(){
+import TextField from '@mui/material/FormControl';
+import { Dialog } from '@mui/material';
 
+function EmailPopup(){
+    return (
+        <Dialog
+          title="Fill in Search Criteria"
+          >
+          <form action="/" method="POST" onSubmit={(e) => { e.preventDefault(); alert('Submitted form!'); this.handleClose(); } }>
+            This dialog spans the entire width of the screen.
+            <TextField name="email" hintText="Email" />
+            <TextField name="FirstName" type="Text" hintText="First Name" />
+            <TextField name="LastName" type="Text" hintText="Last Name" />
+            <TextField name="Title" type="Text" hintText="Job Title" />
+            <TextField name="Location" type="Text" hintText="Location" />
+            <TextField name="Salary" type="Number" hintText="Minimum Salary" />
+          </form>
+        </Dialog>
+      );
 }
 
 export default EmailPopup;
