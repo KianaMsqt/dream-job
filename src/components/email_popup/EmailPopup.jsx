7bc71b1bdc1d116ea9edd3a64f866c99113894f8
Nicholas Brink <nicbrink@gmail.com>
2023-03-14 20:59:10 +0000

    add email form code

--- src/components/email_popup/EmailPopup.jsx
+++ src/components/email_popup/EmailPopup.jsx
@@ -1,5 +1,38 @@
-function EmailPopup(){
-
+function EmailPopup() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField id="outlined-basic" label="Name" variant="outlined" />
+        <TextField id="outlined-basic" label="Surname" variant="outlined" />
+      </div>
+      <div>
+        <TextField
+          id="outlined-basic"
+          label="job description"
+          variant="outlined"
+        />
+        <TextField id="outlined-basic" label="location" variant="outlined" />
+      </div>
+      <div>
+        <TextField
+          id="outlined-basic"
+          label="minimum salary"
+          variant="outlined"
+        />
+      </div>
+      <div>
+        <TextField id="outlined-basic" label="email" variant="outlined" />
+      </div>
+      <Button variant="contained">Save Job Search</Button>
+    </Box>
+  );
 }
 
 export default EmailPopup;
