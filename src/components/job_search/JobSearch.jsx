ef428c0ecf3e82324cb06a41369a7b8e18b83d20
KianaMsqt <masqati.kiana@gmail.com>
2023-03-15 20:55:33 +0000

    Create search query via user inputs

--- src/components/job_search/JobSearch.jsx
+++ src/components/job_search/JobSearch.jsx
@@ -1,28 +1,38 @@
 import React, { useState, useEffect } from "react";
-import "./style.css";
-
-import { fakeApiReturnData, makeFakeApiRequest } from '../../utils/fakeApi';
 import SearchFields from "./SearchFields";
 import SearchResultCard from "./SearchResultCard";
+import "./style.css";
+
+
+import { fakeApiReturnData, makeFakeApiRequest } from '../../utils/fakeApi';
 //import axios from 'axios';
 
 export default function JobSearch() {
 
-  // Request to jsearch and print the results on the console
-  // const options = {
-  //   method: 'GET',
-  //   url: 'https://jsearch.p.rapidapi.com/search',
-  //   params: {
-  //     query: 'Node developer London, UK',
-  //     num_pages: '1'
-  //   },
-  //   headers: {
-  //     'X-RapidAPI-Key': '',
-  //     'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-  //   }
-  // };
 
-  const [ searchResults, setSearchResults ] = useState([]);
+  const [searchQuery, setSearchQuery] = useState([]);
+
+  const [searchResults, setSearchResults] = useState([]);
+
+  // Request to jsearch and print the results on the console
+  // const axios = require("axios");
+
+  // useEffect(()=>{
+  //   const options = {
+  //     method: 'GET',
+  //     url: 'https://jsearch.p.rapidapi.com/search',
+  //     params: {
+  //       query: `${searchQuery}`, 
+  //       page: `${page}`, 
+  //       num_pages: '1'
+  //     },
+  //     headers: {
+  //       'X-RapidAPI-Key': '28a99368c8msh158d941b1a4bd17p16d687jsn5202245154cb',
+  //       'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+  //     }
+  //   }
+  // });
+
   useEffect( ()=> {
     // axios.request(options)
     makeFakeApiRequest(true, 2000, fakeApiReturnData)
@@ -31,11 +41,13 @@ export default function JobSearch() {
     }).catch(function (error) {
       console.error(error);
     });
-  }, [] )
+  }, [searchQuery] );
 
   return(
     <>
-      <SearchFields />
+      <SearchFields 
+        searchQuery={setSearchQuery}
+      />
       {searchResults.map((result)=>{
         return(
           <SearchResultCard key={result.job_id} result={result} />
