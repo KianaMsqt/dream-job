b84eb4473c9bf4534b4b7ff44f7759cebf235992
Erickuby <ericcnwankwo@gmail.com>
2023-03-17 19:03:18 +0000

    Styling added to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,36 @@
+import React from "react";
 import { BottomNavigation } from "@mui/material";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import TimelineIcon from "@mui/icons-material/Timeline";
-export default function Footer() {
+import "./style.css";
 
+const Footer = () => {
   return (
-    <BottomNavigation> 
-  
-      <p className="pcontainer">
-          Produced by -- Kiana -- Eric -- Nick -- Ope ©
-          <div>
-            <BottomNavigationAction label="GitHub" to='https://github.com/KianaMsqt/dream-job' icon={<GitHubIcon />} />
-            <BottomNavigationAction label="Timeline" to='https://github.com/KianaMsqt/dream-job/branches' icon={<TimelineIcon />} />
-          </div>
-        </p>
-        
-    </BottomNavigation>
+    <div className="footer-container p-3 bg-secondary text-light">
+      <p className="m-0 text-center fw-bold fs-5">
+        Produced by Kiana, Eric, Nick, Ope &copy; Dream Job Finder
+      </p>
+      <BottomNavigation showLabels className="justify-content-end d-flex mt-2">
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+          <BottomNavigationAction
+            label="GitHub"
+            href="https://github.com/KianaMsqt/dream-job"
+            target="_blank"
+            rel="noopener noreferrer"
+            icon={<GitHubIcon />}
+          />
+          <BottomNavigationAction
+            label="Timeline"
+            href="https://github.com/KianaMsqt/dream-job/branches"
+            target="_blank"
+            rel="noopener noreferrer"
+            icon={<TimelineIcon />}
+          />
+        </div>
+      </BottomNavigation>
+    </div>
   );
-}
+};
+
+export default Footer;
--- src/components/navlink/Header.jsx
+++ src/components/navlink/Header.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import "./Header.css";
 
 function Header() {
   return (
