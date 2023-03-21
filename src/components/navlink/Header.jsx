04a55cb73ca2b8a61df5903a1ced940e575242eb
Nicholas Brink <nicbrink@gmail.com>
2023-03-21 21:13:08 +0000

    removed homepage nav link

--- src/components/navlink/Header.jsx
+++ src/components/navlink/Header.jsx
@@ -1,83 +1,76 @@
-import * as React from 'react';
+import * as React from "react";
 import { NavLink } from "react-router-dom";
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import logo from "../../assets/logo.png";
 
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ position: 'relative', backgroundColor: '#e1f5fe' }}>
+      <AppBar
+        position="static"
+        sx={{ position: "relative", backgroundColor: "#e1f5fe" }}
+      >
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <a className="navbar-brand" href="/">
-            <img src={logo} alt="Logo" style={{ height: "50px", zIndex: 'tooltip' }} />
-          </a>
+            <a className="navbar-brand" href="/">
+              <img
+                src={logo}
+                alt="Logo"
+                style={{ height: "50px", zIndex: "tooltip" }}
+              />
+            </a>
           </Typography>
-         
-                <NavLink
-                  to="/"
-                  style={{
-                    color: "#1338BE",
-                    fontWeight: 'bolder',
-                    padding: "15px",
-                    textDecoration: 0,
-                  }}
-                >
-                  Job Search
-                </NavLink>
-              
-                <NavLink
-                  to="/contact"
-                  style={{
-                    color: "#1338BE",
-                    fontWeight: 'bolder',
-                    padding: "15px",
-                    textDecoration: 0,
-                  }}
-                >
-                  Contact
-                </NavLink>
-              
-              
-                <NavLink
-                  to="/subscribe"
-                  style={{
-                    color: "#1338BE",
-                    fontWeight: 'bolder',
-                    padding: "15px",
-                    textDecoration: 0,
-                  }}
-                >
-                  Subscribe
-                </NavLink>
-                <NavLink
-                  to="/careersupport"
-                  style={{ 
-                    color: "#1338BE",
-                    fontWeight: 'bold',
-                    padding: "15px",
-                    textDecoration: 0,
-                  }}
-                  end
-                >
-                  Career
-                </NavLink>
-                <NavLink
-                  to="/homehero"
-                  style={({ isActive }) => ({
-                    color: "red",
-                    background: "light-blue",
-                    padding: "10px",
-                  })}
-                  end
-                >
-                  Homepage
-                </NavLink>
-                
-         
+
+          <NavLink
+            to="/"
+            style={{
+              color: "#1338BE",
+              fontWeight: "bolder",
+              padding: "15px",
+              textDecoration: 0,
+            }}
+          >
+            Job Search
+          </NavLink>
+
+          <NavLink
+            to="/contact"
+            style={{
+              color: "#1338BE",
+              fontWeight: "bolder",
+              padding: "15px",
+              textDecoration: 0,
+            }}
+          >
+            Contact
+          </NavLink>
+
+          <NavLink
+            to="/subscribe"
+            style={{
+              color: "#1338BE",
+              fontWeight: "bolder",
+              padding: "15px",
+              textDecoration: 0,
+            }}
+          >
+            Subscribe
+          </NavLink>
+          <NavLink
+            to="/careersupport"
+            style={{
+              color: "#1338BE",
+              fontWeight: "bold",
+              padding: "15px",
+              textDecoration: 0,
+            }}
+            end
+          >
+            Career
+          </NavLink>
         </Toolbar>
       </AppBar>
     </Box>
