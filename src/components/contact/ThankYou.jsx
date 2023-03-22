83c7f7f1fd7b9d42294c1ee95123f5b5984ac8a8
Ope <o_orekoya@yahoo.com>
2023-03-22 19:34:14 +0000

    added image to thank you page

--- src/components/contact/ThankYou.jsx
+++ src/components/contact/ThankYou.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { CardMedia } from '@mui/material';
 
 export default function ThankYou(){
     return (
-        <div className="contact-container">
+    <div className="contact-container">
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
         <h1 style={{ fontSize: '36px', fontWeight: 'bold', marginBottom: '20px' }}>Thank You</h1>
         <div style={{ width: '600px' }}>
           <p style={{ fontSize: '16px', lineHeight: '26px', marginBottom: '20px', textAlign: 'center' }}>
-            Thanks for Contacting Us. We will endeavour to reply to your query as soon as possible.
+            We will endeavour to reply to your query as soon as possible.
           </p>
           
         </div>
       </div>
-     
+      <CardMedia
+          component="img"
+          height="600"
+          image={require("./blueMountain.jpg")}
+        />
     </div>
     )
 }
