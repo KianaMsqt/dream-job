2de66eb89be19cd7ea317fcea7882c6207ee5a2d
Erickuby <ericcnwankwo@gmail.com>
2023-03-22 16:28:44 +0000

    Contact page styled

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,24 +1,33 @@
-import { BottomNavigation, Typography } from "@mui/material";
+import { BottomNavigation, Typography, useTheme } from "@mui/material";
 import Paper from "@mui/material/Paper";
 
-
 export default function Footer() {
+  const theme = useTheme();
 
   return (
     <BottomNavigation> 
-  
-      <Paper sx={{marginTop: 'calc(15% + 60px)',
-        width: '100%',
-        position: 'fixed',
-        bottom: 0,
-        width: '100%',
-        backgroundColor: '#e1f5fe' 
-        }} component="footer" square variant="outlined">
-        <Typography variant="caption" color="#1338BE" padding={50}>
+      <Paper
+        sx={{
+          backgroundColor: theme.palette.primary.light,
+          color: theme.palette.primary.contrastText,
+          position: 'fixed',
+          bottom: 0,
+          width: '100%',
+          padding: theme.spacing(2),
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        component="footer"
+        square
+      >
+        <Typography
+          variant="caption"
+          sx={{ fontFamily: 'Roboto' }}
+        >
           Produced by -- Kiana -- Eric -- Nick -- Ope ©
         </Typography>
       </Paper>
-        
     </BottomNavigation>
   );
 }
